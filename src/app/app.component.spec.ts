75b560efc042808d2c94009e887aa3d66b016613
Wojciech Nowicki <nowiro@gmail.com>
2021-03-21 16:56:40 +0100

    as

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'mf-host'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mf-host');
+    expect(app.title).toEqual('mf-host122');
   });
 
   it('should render title', () => {
